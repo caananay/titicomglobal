4374d55bd8f21a8e9e586ac2792f67a61c245d35
Ca-ananay <caananay.maduka@gmail.com>
2017-09-12 15:32:06 +0100

    Procfile added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn titicom.wsgi:application
--- titicom/settings.py
+++ titicom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=-ri#6^!q&qe@osucvqyd^)t0fzf1+ru53c^%two50ye%d)++m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ea819595.ngrok.io']
+ALLOWED_HOSTS = ['127.0.0.1', 'titicom-global.herokuapp.com']
 
 LOGIN_URL = '/login'
 # Application definition
@@ -162,7 +162,7 @@ EMAIL_USE_TLS = True
 CART_SESSION_ID = 'cart'
 
 #PayPal Settings
-# SITE_URL= ''
-PAYPAL_NOTIFY_URL = ' http://ea819595.ngrok.io'
+SITE_URL= 'titicom-global.herokuapp.com'
+PAYPAL_NOTIFY_URL = 'titicom-global.herokuapp.com'
 PAYPAL_RECEIVER_EMAIL ='doncanny-facilitator@yahoo.com'
-PAYPAL_TEST= True
+# PAYPAL_TEST= True
