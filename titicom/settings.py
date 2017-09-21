232a7b2a10c180761087e7f8adc7ceab90e5ce30
Ca-ananay <caananay.maduka@gmail.com>
2017-09-21 13:55:59 +0100

    modified settings

--- titicom/settings.py
+++ titicom/settings.py
@@ -157,8 +157,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CART_SESSION_ID = 'cart'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'titiphotos593@gmail.com'
+EMAIL_HOST_PASSWORD = 'Bl@zetr@il'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
