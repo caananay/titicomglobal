fd8ad39f097f0be1c1bd4449bf2cfe257decc57d
Ca-ananay <caananay.maduka@gmail.com>
2017-10-11 10:47:43 +0100

    add secret keys file

---
+++ titicom/secret_keys.py
@@ -0,0 +1,16 @@
+#AWS settings
+AWS_STORAGE_BUCKET_NAME = 'titicomglobal'
+AWS_S3_REGION_NAME = 'eu-west-2'  # e.g. us-east-2
+AWS_ACCESS_KEY_ID = 'AKIAIQV5OYFEVOZBC5JQ'
+AWS_SECRET_ACCESS_KEY = 'xNqG+KmY+klCkQZEEudRyLsEV3u+Sbs3HMR5RWLB'
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+#Email settings
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'titiphotos593@gmail.com'
+EMAIL_HOST_PASSWORD = 'Bl@zetr@il'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
