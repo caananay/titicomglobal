67001295ea7bc8c5d1deb168557f8f4fb1db2838
Ca-ananay <caananay.maduka@gmail.com>
2017-10-17 21:04:32 +0100

    modified blog forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(label='From', max_length=25, disabled=True)
+    name = forms.CharField(label='From', max_length=25, widget=forms.HiddenInput())
     email = forms.EmailField(widget=forms.HiddenInput())
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
