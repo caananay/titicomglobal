3099d7e2d2b79f29eb91edf472dd92b6233818dc
Ca-ananay <caananay.maduka@gmail.com>
2017-10-17 18:11:04 +0100

    working on connecting to AWS using config files stored on heroku

--- titicom/secret_keys.py
+++ titicom/secret_keys.py
@@ -1,11 +1,3 @@
-
- 
-
- # AWS_ACCESS_KEY_ID = 'AKIAIQV5OYFEVOZBC5JQ'
-# AWS_SECRET_ACCESS_KEY = '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'
- # Tell django-storages the domain to use to refer to static files.
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
 #Email settings
 
 EMAIL_HOST = 'smtp.gmail.com'
